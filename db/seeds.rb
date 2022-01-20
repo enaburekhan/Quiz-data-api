498acb7200a116359c922748573180ea3fc4aad4
enaburekhan <ericenaburekhan2018@gmail.com>
2022-01-20 19:13:22 +0100

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'eric@gmail.com', password: 'eric123')
+
+QuizDatum.create(founder_name: 'Oluwatoyin', email: 'toyin@gmail.com', country: 'nigeria',
+                                gender: 'female', race_of_founder: 'black',
+                                text_box: 'other credentials include vegan, etc',
+                                conscious_beauty_credentials: 'sulphate_free', user_id: 1)
--- spec/requests/quiz_data_spec.rb
+++ spec/requests/quiz_data_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'QuizData', type: :request do
   before(:example) do
     @user = User.create(email: 'eric@gmail.com', password: 'eric123')
     @uiz_data = QuizDatum.create(founder_name: 'Oluwatoyin', email: 'toyin@gmail.com', country: 'nigeria',
-                                gender: 'female', race_of_founder: 'black',
-                                text_box: 'other credentials include vegan, etc',
-                                conscious_beauty_credentials: 'sulphate_free', user_id: 1)
+                                 gender: 'female', race_of_founder: 'black',
+                                 text_box: 'other credentials include vegan, etc',
+                                 conscious_beauty_credentials: 'sulphate_free', user_id: 1)
     token = JWT.encode({ user_id: @user.id }, Rails.application.secret_key_base)
     @headers = { Authorization: "Bearer #{token}" }
   end
