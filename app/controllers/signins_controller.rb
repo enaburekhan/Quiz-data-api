92ab9484acdbd4047f3ca486578984e1161d594f
enaburekhan <ericenaburekhan2018@gmail.com>
2022-01-20 02:30:51 +0100

    Implement signin for existing user

--- app/controllers/signins_controller.rb
+++ app/controllers/signins_controller.rb
@@ -1,2 +1,12 @@
 class SigninsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      payload = { user_id: user.id }
+      token = encode_token(payload)
+      render json: { email: user.email, user_id: user.id, jwt: token }
+    else
+      render json: { failure: 'Login failed! Invalid email/password combination.' }, status: :not_acceptable
+    end
+  end
 end
